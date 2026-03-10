48541948affd475f0d184b6774066a1f51b4c908
SL-Shff <samuelfernandesfreire@gmail.com>
2026-03-10 10:49:35 -0400

    Finalizei o desfaio de super trunfo de nivel novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+char Estado[3];
+char Codigo[5];
+char Cidade[50];
+int Habitantes;
+float Area;
+float PIB;
+int Turismo;
+
+printf("Digite a Sigla do estado: \n");
+scanf("%s", Estado);
+
+printf("Digite o Codigo: \n");
+scanf("%s", Codigo);
+
+printf("Digite a Cidade: \n");
+scanf("%s", Cidade);
+
+printf("digite o numero de habitantes: \n");
+scanf("%i", &Habitantes);
+
+printf("Digite a Area: \n");
+scanf("%f", &Area);
+
+printf("Digite o PIB: \n");
+scanf("%f", &PIB);
+
+printf("Digite a quantia de pontos turisticos: \n");
+scanf("%i", &Turismo);
+
+printf("Estado: %s\n", Estado);
+printf("Codigo: %s\n", Codigo);
+printf("Cidade: %s\n", Cidade);
+printf("Numero de habitantes: %i\n", Habitantes);
+printf("Area: %f\n", Area);
+printf("PIB: %f\n", PIB);
+printf("Turismo: %i\n", Turismo);
+
+return 0;
+
+}// Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     // Cadastro das Cartas:
